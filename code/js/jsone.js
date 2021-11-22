07a3bfcdeea7c9aa5a83e94c5e3565be1c35e6c9
Eric Klopper <erictklopper@gmail.com>
2021-11-22 14:48:18 +0200

    20211221448

---
+++ code/js/jsone.js
@@ -0,0 +1,92 @@
+
+
+var a = 9;
+
+var myFirstName = "Eric";
+var mySecondName = "Torr";
+var myLastName = "Klopper";
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+/* 
+var vone = 5;
+var vone = 10;
+console.log(vone);
+
+the second var vone decleration overwrites the previous one
+*/
+
+let vone = 5;
+let vone = 10;
+
+/* similar to var let is used to assign a value (to vone) but the second decleration produces a error because let can only be used once per variable
+*/
+
+const VONE = "value"; /* const assigns the value permanently */
+/* name constant (const) values using only UPPERCASE LETTERS */
+
+const sum = 10 + 10;
+const minus = 45 - 33;
+const product = 8 * 10;
+const quotient = 66 / 33;
+myVar++;
+myVar--;
+const myDecimalNumber = 5.7;
+const product = 2.0 * 2.5;
+const quotient = 4.4 / 2.0;
+const remainder = 11 % 3;
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a = a += 12;
+b = b += 9;
+c = c += 7;
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a = a -= 6;
+b = b -= 15;
+c = c -= 1;
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a = a *= 5;
+b = b *= 3;
+c = c *= 10;
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+const myStr = "This is the start. " + "This is the end."; // Change this line
+
+
+
